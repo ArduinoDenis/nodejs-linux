7137c47b1adc867f268821dd6dc76d529ca7abfd
arduinodenis <arduinodenis1@gmail.com>
2022-10-31 12:07:05 +0100

    upload installer.sh

---
+++ installer.sh
@@ -0,0 +1,10 @@
+echo "aggiorno la lista dei pacchetti"
+sudo apt-get update -y;
+echo "installo nodejs"
+sudo apt-get install nodejs -y;
+echo "versione di nodejs"
+nodejs -v;
+echo "installo il gestione dei pacchetti di nodejs"
+sudo apt-get install npm -y;
+echo "numero della verione del gestore dei pacchetti"
+npm -v;
