da999a06bccefb195728feac2c2df5e22d798ef0
arduinodenis <90153974+ArduinoDenis@users.noreply.github.com>
2024-02-17 19:14:45 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
 echo "aggiorno la lista dei pacchetti"
 sudo apt-get update -y;
 echo "installo nodejs"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
